43eb5709b0e39f64b5c5666d7eb09c95896c6930
Matheus3575 <matheuscoelho357512@gmail.com>
2025-02-21 13:49:24 -0300

    add: Banco de dados MySQL

---
+++ sistema/src/main/java/br/aluno/uece/sistema/controller/HomeController.java
@@ -0,0 +1,15 @@
+//Controller de exemplo:
+
+package br.aluno.uece.sistema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
